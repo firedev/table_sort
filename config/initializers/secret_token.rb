988d6da5970cbf1122011238b63bc9608296105d
Nick <nick@firedev.com>
2013-08-06 13:55:07 +0700

    empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TableSort::Application.config.secret_key_base = 'c5661b88b626573961b4330ea230a765d16503fc469881b7a729bfc6988956b72dd11ab334315b7e6edad1e1db4d4cd5eab213049fdcc96e066dc7c4c10f7496'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TableSort::Application.config.session_store :cookie_store, key: '_table_sort_session'
